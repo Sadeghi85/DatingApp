92953d371547dec8e5de26105e3a053a8641f375
Sadeghi <sadeghi85@hotmail.com>
2022-02-21 14:29:37 +0330

    .

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Dating App';
+export class AppComponent implements OnInit {
+  title = 'The Dating App';
+  users: any;
+
+  constructor (private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('http://localhost:5000/api/users').subscribe({
+      complete: () => {  }, // completeHandler
+      error: (error) => { console.log(error); },    // errorHandler 
+      next: (response) => { this.users = response; },     // nextHandler
+    });
+  }
 }
