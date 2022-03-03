b6f9649df32afddb8b5efe10d25c2b9f9bab16cf
Sadeghi <sadeghi85@hotmail.com>
2022-03-03 19:24:29 +0330

    .

--- API/Startup.cs
+++ API/Startup.cs
@@ -39,6 +39,8 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+
+			services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,6 +58,10 @@ namespace API
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+
+			app.UseCors(configurePolicy => {
+				configurePolicy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:4200");
+			});
 
             app.UseAuthorization();
 
