f3090b2f64e811965235192ae1d8539b2e488e07
Sadeghi <sadeghi85@hotmail.com>
2022-03-13 14:44:37 +0330

    .

---
+++ API/Interfaces/ITokenService.cs
@@ -0,0 +1,11 @@
+using API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Interfaces {
+	public interface ITokenService {
+		string CreateToken(User user);
+	}
+}
